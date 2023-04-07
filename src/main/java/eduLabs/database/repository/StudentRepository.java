a775c295ee9cff59517fae008bcf4b3094b8625f
Shakila K Bandara <skbandara32@gmail.com>
2023-04-07 19:51:58 +0530

    Removed some unnecessary files

--- src/main/java/eduLabs/database/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package eduLabs.database.repository;
-
-import eduLabs.database.model.DuePaymentsModel;
-import eduLabs.database.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-
-}
